952b4086f167c42761c8433ea5189c8f5fcaa1aa
Ibrahem_Owais <elelnesr3@gmail.com>
2026-08-25 02:31:22 +0300

    Day-12

---
+++ Day-12/js/main.js
@@ -0,0 +1,80 @@
+// ================================================
+
+// 1-OBJECT
+
+// ================================================
+
+// let person = {
+//     fullname: 'Ibrahim',
+//     age: 22,
+//     gender: 'Male',
+//     job: 'Front-End',
+//     salary: 160000,
+//     city: 'Cairo',
+//     isStudent: true,
+//     wife: {
+//         fullname: 'Haneen',
+//         age: 21,
+//         gender: 'Female',
+//         son: {
+//             fullname: 'Essam',
+//             age: 4,
+//             gender: 'Male',
+//         }
+//     }
+// }
+
+// console.log(person.fullname)
+// console.log(person.jop)
+// console.log(person.wife.age)
+// console.log(person.wife.son.fullname)
+
+// ================================================
+
+//2-FUNCTION
+
+// ================================================
+
+// function calculateDiscount(price, discount) {
+//     if (discount > 0) {
+//         return (price - discount);
+//     } else if (discount == 0) {
+
+//     }
+//     return price;
+// }
+// console.log(calculateDiscount(1000, 50))
+// var result = calculateDiscount(1000, 25);
+// console.log(result);
+
+
+// ================================================
+
+//2-LOOPS
+
+// ================================================
+
+// for (var x = 0; x<= 10; x++) {
+//     console.log(x)
+// }
+
+
+// var y = 0
+// while (y <= 10) {
+//     console.log(y);y++;
+// }
+
+
+// var z = 0;
+// do {
+//     console.log(z);
+//     z++;
+// }
+// while (z <= 6);
+
+
+// function getAvg(num1, num2) {
+//     console.log((num1 + num2) / 2);
+// }
+
+// getAvg(10, 20)
